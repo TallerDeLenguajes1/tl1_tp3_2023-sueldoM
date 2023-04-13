6a2e770eca24fb5057a180e10ded477eaa64fff0
“SueldoMariano” <sueldo.011.ms@gmail.com>
2023-04-13 19:33:44 -0300

    punto 3 modificado

--- tp3_3.c
+++ tp3_3.c
@@ -2,39 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_NOMBRES 5
-#define MAX_LONG_NOMBRE 50
-
 int main() {
-    char *nombres[MAX_NOMBRES]; // Vector de punteros a char
-    char nombre[MAX_LONG_NOMBRE];
+    char* nombres[5];
+    char buffer[50];
     int i;
 
-    // Reservar memoria para cada puntero del vector
-    for (i = 0; i < MAX_NOMBRES; i++) {
-        nombres[i] = (char*)malloc(MAX_LONG_NOMBRE * sizeof(char));
-        if (nombres[i] == NULL) {
-            printf("Error al reservar memoria.\n");
-            return 1;
-        }
-    }
-
-    // Solicitar y cargar los nombres en el vector
-    for (i = 0; i < MAX_NOMBRES; i++) {
+    for (i = 0; i < 5; i++) {
         printf("Ingrese el nombre %d: ", i+1);
-        fgets(nombre, MAX_LONG_NOMBRE, stdin); // Leer el nombre desde la entrada estándar
-        nombre[strlen(nombre)-1] = '\0'; // Eliminar el salto de línea al final del nombre
-        strcpy(nombres[i], nombre); // Copiar el nombre al vector de punteros
+        fgets(buffer, sizeof(buffer), stdin);
+
+        nombres[i] = (char*) malloc(strlen(buffer)+1);
+        strcpy(nombres[i], buffer);
     }
 
-    // Listar los nombres por pantalla
-    printf("\nNombres ingresados:\n");
-    for (i = 0; i < MAX_NOMBRES; i++) {
-        printf("%d. %s\n", i+1, nombres[i]);
-    }
-
-    // Liberar la memoria reservada para cada puntero del vector
-    for (i = 0; i < MAX_NOMBRES; i++) {
+    printf("Los nombres ingresados son:\n");
+    for (i = 0; i < 5; i++) {
+        printf("%d: %s", i+1, nombres[i]);
         free(nombres[i]);
     }
 
